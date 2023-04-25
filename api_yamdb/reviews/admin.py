f46c56f85f7f2c3acc49546e1a59de5be6cff8d1
Dmitry Guryev <dmitry.guryev@gmail.com>
2023-04-25 12:47:59 +0300

    Add initial models

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Genre, Title
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'slug',
+    )
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'slug',
+    )
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'year',
+        'description',
+        'get_genres',
+        'category'
+    )
+    list_editable = ('category',)
+    search_fields = ('name', 'description',)
+
+    def get_genres(self, obj):
+        return ', '.join([str(genre) for genre in obj.genres.all()])
+
+    get_genres.short_description = 'genres'
+
+
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Genre, GenreAdmin)
+
+admin.site.register(Title, TitleAdmin)
